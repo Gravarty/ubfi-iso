c91d6b85de79f4cdfa63e58fcdad9af308c2b2ae
Gravarty <Gravarty@gmail.com>
2026-09-12 09:00:35 +0200

    Update ubfi.sh

--- ubfi.sh
+++ ubfi.sh
@@ -11,7 +11,7 @@
 # ## Sonst erkennt der eingebaute Updater keine neue       ##
 # ## Version. Format: Punktzahl, in doppelten "".          ##
 # ##########################################################
-ubfiversion="0.8"
+ubfiversion="0.9"
 
 apptitle="Ubuntu Fast Install (ubfi)"
 MOUNTPOINT="/mnt"
@@ -1536,8 +1536,9 @@ pkg_desktop() {
 
     if [ "$sel" = "gnome-shell" ]; then
         local lang_code="${locale_val%%_*}"
-        local gnome_pkgs="gnome-shell gnome-session gdm3 gnome-terminal nautilus gnome-text-editor file-roller gnome-calculator gnome-disk-utility gnome-screenshot eog gnome-tweaks gnome-shell-extension-manager fonts-noto gstreamer1.0-plugins-good gstreamer1.0-plugins-base gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-libav gstreamer1.0-vaapi gnome-system-monitor language-pack-gnome-${lang_code} language-pack-gnome-${lang_code}-base cifs-utils"
+        local gnome_pkgs="gnome-shell gnome-session gdm3 gnome-terminal nautilus gnome-text-editor file-roller gnome-calculator gnome-disk-utility gnome-screenshot eog gnome-tweaks gnome-shell-extension-manager fonts-noto gstreamer1.0-plugins-good gstreamer1.0-plugins-base gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-libav gnome-system-monitor language-pack-gnome-${lang_code} language-pack-gnome-${lang_code}-base cifs-utils"
         local gnome_bloat="yelp* yaru-theme-gnome-shell"
+        [ "${UBUNTU_RELEASE}" != "stonking" ] && gnome_pkgs="$gnome_pkgs gstreamer1.0-vaapi"
         if ! chroot /mnt apt install -y $gnome_pkgs; then
             pressanykey
             return
